3cfec8acf34a03fc2ac551a2f80255d9b167b845
trader TGN <trader@tcn.local>
2021-05-17 12:42:55 +0200

    2 commit

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
 // });
 
 function openNav() {
-  document.getElementById('mySidenav').style.width = '70vw';
+  document.getElementById('mySidenav').style.width = '75vw';
 }
 
 function closeNav() {
