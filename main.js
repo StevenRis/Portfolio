615f53d9008a8a56e58a4c3421b58248a9b6aea9
example@gmail.com <s.griszko@gmail.com>
2021-05-29 20:54:30 +0200

    add social icons

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 const navigation = document.querySelector('.navigation');
 const menuBtn = document.querySelector('.menu-btn__burger, .nav-item');
 const navItem = document.querySelector('.nav-item');
+const socialIcons = document.querySelector('.social-icons');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open');
     navigation.classList.add('active');
+    socialIcons.classList.add('active');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
     navigation.classList.remove('active');
+    socialIcons.classList.remove('active');
     menuOpen = false;
   }
 });
