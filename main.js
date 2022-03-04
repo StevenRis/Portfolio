07754edfa6e973f5a3b54057db297bbb95b8b4e3
Stanisław Griszko <s.griszko@gmail.com>
2022-03-04 23:34:53 +0100

    rebuilt language change

--- main.js
+++ main.js
@@ -120,6 +120,217 @@ function languages(h2, para) {
   skillsPara.textContent = 'w których mówię';
 }
 
+function createProjects() {
+  const container = document.querySelector('.portfolio_section');
+
+  container.innerHTML = `
+  <div class="portfolio__projects-title">
+    <h2>Projekty</h2>
+    <p>które stworzyłem</p>
+  </div>
+  <div class="portfolio-projects__container reverse">
+  <div class="projects-image project__one">
+    <img src="images/race_place.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>Race_Place</h3>
+    <h4>Javascript</h4>
+    <p>Projekt poświęcony grze Dirt Rally 2.0</p>
+    <p>
+      Obecnie jest w fazie rozwoju. Celem jest zebranie konfiguracji
+      samochodów w jednym miejscu w celu szybszego ustawienia samochodu
+      na etap rajdu
+    </p>
+    <div>
+      <a
+        class="btn"
+        href="https://stevenris.github.io/race_place/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/race_place"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="portfolio-projects__container">
+  <div class="projects-image project__one">
+    <img src="images/note-keeper-app.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>Note Keeper</h3>
+    <h4>React, CSS</h4>
+    <p>Notatnik</p>
+    <div>
+      <a
+        class="btn"
+        href="https://loving-haibt-a002d5.netlify.app/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/Note-Keeper"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="portfolio-projects__container reverse">
+  <div class="projects-image project__one">
+    <img src="images/weather-app.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>Weather App</h3>
+    <h4>React, CSS</h4>
+    <p>Aplikacja korzysta z API pogody</p>
+    <div>
+      <a
+        class="btn"
+        href="https://musing-brown-fb90d2.netlify.app/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/Weather-app"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="portfolio-projects__container">
+  <div class="projects-image project__one">
+    <img src="images/to-do-list.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>To Do List</h3>
+    <h4>React, CSS</h4>
+    <p>
+      Lista zadań - web aplikacja napisana z użyciem frameworku React
+    </p>
+    <div>
+      <a
+        class="btn"
+        href="https://stevenris.github.io/ToDoList/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/ToDoList"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="portfolio-projects__container reverse">
+  <div class="projects-image">
+    <img src="images/game.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>Rock, Paper, Scissors Game</h3>
+    <h4>JavaScript, CSS</h4>
+    <p>Projekt został oparty na znanej grze Kamień, Papier, Nożyczki</p>
+    <div>
+      <a
+        class="btn"
+        href="https://stevenris.github.io/Game-rock-paper-scissors/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/Game-rock-paper-scissors"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="portfolio-projects__container">
+  <div class="projects-image project__one">
+    <img src="images/date.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>DatePet</h3>
+    <h4>HTML, CSS, JS</h4>
+    <p>Portal randkowy dla zwierzaków</p>
+    <div>
+      <a
+        class="btn"
+        href="https://stevenris.github.io/DatePet/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/DatePet"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+<div class="portfolio-projects__container reverse">
+  <div class="projects-image">
+    <img src="images/port.png" alt="" />
+  </div>
+  <div class="project-description">
+    <h3>Portfolio website</h3>
+    <h4>Bootstrap</h4>
+    <p>Strona stworzona jako portfolio fotografa</p>
+    <div>
+      <a
+        class="btn"
+        href="https://stevenris.github.io/Photographer-portfolio/"
+        target="_blank"
+        >Demo</a
+      >
+      <a
+        class="btn"
+        href="https://github.com/StevenRis/Photographer-portfolio"
+        target="_blank"
+        >Code</a
+      >
+    </div>
+  </div>
+</div>
+
+  `;
+}
+
+function contact(h2, para, n, p, m, btn) {
+  const parag = document.querySelector(para),
+    title = document.querySelector(h2),
+    name = document.querySelector(n),
+    phone = document.querySelector(p),
+    message = document.querySelector(m),
+    button = document.querySelector(btn);
+
+  title.textContent = 'Kontakt';
+  parag.textContent = 'Skontaktuj się ze mną';
+  name.textContent = 'Twoje imię';
+  phone.textContent = 'Telefon';
+  message.textContent = 'Wiadomość';
+  button.innerHTML = `
+  <img src="images/send-mail.png" alt="" />Wyślij
+  `;
+}
+
 const lang = document.querySelector('.lang-pl');
 
 lang.addEventListener('click', (e) => {
@@ -129,4 +340,14 @@ lang.addEventListener('click', (e) => {
   changeTech('.skills__tech-title h2', '.skills__tech-title p');
   aboutMe('.about-me__container h2', '.about-me__container p');
   languages('.skills__lang-title h2', '.skills__lang-title p');
+
+  createProjects();
+  contact(
+    '.contact-form h2',
+    '.p__text',
+    '.name',
+    '.phone',
+    '.message',
+    '.btn-form'
+  );
 });
